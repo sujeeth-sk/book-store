f38d42e3c6b67c26dbf0c0d6f69864bf546e7ebd
Sujeeth Kompella <131647802+sujeeth-sk@users.noreply.github.com>
2024-10-02 18:40:41 +0530

    Delete

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const mongoDB_URL = "mongodb+srv://sujeeth2004:6JXNvpoqCVxzgZnO@cluster0.5vd1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// export const mongoDB_URL = "mongodb://localhost:27017/";
